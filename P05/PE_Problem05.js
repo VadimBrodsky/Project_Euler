04bf9318c7463b0fc833858cdb688f3b59a7f470
Vadim Brodsky <vadim.brodsky@gmail.com>
2012-09-03 11:47:37 -0400

    Solved the fifth problem

--- P05/PE_Problem05.js
+++ P05/PE_Problem05.js
@@ -8,7 +8,30 @@
 var timer = require("../timer.js");
 timer.startTimer();
 
+var isDivisable = function (min, max, number) {
+	var flag = true;
+	for (min; min <= max; min++) {
+		if (flag === true && number % min === 0) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+	}
+	return flag;
+};
+
+var smallestDivisable = function (min, max) {
+	var number = 1, flag = false;
+	while (!flag) {
+		if (isDivisable(min, max, number)) {
+			flag = true;
+		} else {
+			number++;
+		}
+	}
+	return number;
+};
 
 
-
+console.log(smallestDivisable(1, 20));
 timer.endTimer();
