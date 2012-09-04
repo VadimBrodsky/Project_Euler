997a636c9586402b76f31973dc80b878f242e518
Vadim Brodsky <vadim.brodsky@gmail.com>
2012-09-04 18:20:25 -0400

    Solved the 6th problem

--- P06/PE_Problem06.js
+++ P06/PE_Problem06.js
@@ -8,7 +8,25 @@
 var timer = require("../timer.js");
 timer.startTimer();
 
+function sumOfSquares (range) {
+	var sum = 0;
+	for (var i = 1; i <= range; i++) {
+		sum = sum + Math.pow(i, 2);
+	}
+	return sum;
+};
 
+function squareOfSum (range) {
+	var sum = 0;
+	for (var i = 1; i <= range; i++) {
+		sum = sum + i;
+	}
+	return Math.pow(sum, 2);
+};
 
+function difference (range) {
+	return squareOfSum(range) - sumOfSquares(range);
+}
 
+console.log(difference(100));
 timer.endTimer();
