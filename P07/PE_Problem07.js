88dd13dd4fa55307d6afe80c4d2cb8bfeb8245e0
Vadim Brodsky <vadim.brodsky@gmail.com>
2012-09-04 22:20:57 -0400

    Solved the 7th problem.

--- P07/PE_Problem07.js
+++ P07/PE_Problem07.js
@@ -8,7 +8,29 @@
 var timer = require("../timer.js");
 timer.startTimer();
 
+var isPrime = function (number) {
+	for (var i=2 ; i < number; i++) {
+		if(number % i === 0) {
+			return false;
+		}
+	}
+	return true;
+};
 
+var primeByIndex = function (index) {
+	var prime = 0;
+	var num = 2;
+	while (index > 0) {
+		if (isPrime(num)) {
+			prime = num;
+			index--;
+			num++;
+		} else {
+			num++;
+		}
+	}
+	return prime;
+};
 
-
+console.log(primeByIndex(10001));
 timer.endTimer();
